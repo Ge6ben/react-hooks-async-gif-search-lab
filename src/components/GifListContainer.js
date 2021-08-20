d96c5d7525e165323574f14a25a5873d3c3f3c55
Ge6ben <dl22gesh40@gmail.com>
2021-08-20 21:34:09 +0300

    Final Touch

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -6,28 +6,26 @@ export default function GifListContainer() {
   const [querySearch, setQuerySearch] = useState("panda");
   const [data, setData] = useState([]);
   const API_Key = "ef9FSon5QRV38w5qZRvLnqCaBU28rCrW";
+
   useEffect(() => {
     fetch(
-      `https://api.giphy.com/v1/gifs/search?q=${querySearch}%20QUERY%20HERE&api_key=${API_Key}&rating=g&limit=3`
+      `https://api.giphy.com/v1/gifs/search?q=${querySearch}&api_key=${API_Key}&rating=g&limit=3`
     )
       .then((res) => res.json())
-      .then(({ data }) => {
-        const gifData = data.map((imgUrl) => ({
-          url: imgUrl.images.original.url,
-        }));
-        setData(gifData);
-        //   console.log(data);
-      })
-      .catch((err) => console.log(err));
+
+        .then(({ data }) =>
+        setData(data.map((gif) => ({ url: gif.images.original.url })))
+      )
+      .catch((error) => console.log(error));
   }, [querySearch]);
 
-  console.log(data);
   return (
     <div style={{ display: "flex" }}>
       {data.map((gif) => (
         <GifList key={gif.id} gifImg={gif.url} />
       ))}
-      <GifSearch setQuerySearch={setQuerySearch} />
+
+          <GifSearch setQuerySearch={ setQuerySearch}/>
     </div>
   );
 }
